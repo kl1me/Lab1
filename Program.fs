dc625d34e7abd27982eea61eea3a9bc111432be2
kl1me <56569962+kl1me@users.noreply.github.com>
2026-02-27 08:20:11 +0500

    Lab1_1
    Lab1_1

--- Program.fs
+++ Program.fs
@@ -1,20 +1,35 @@
 ﻿open System
+let rec CharInput () = 
+    printf "Введите символ: "
+    let x = Console.ReadLine()
+    if ((String.length x)>1) then
+        printfn "Ошибка: Введено больше одного символа!"
+        CharInput ()
+    else
+        x
+
+let ListInput =
+    let x = CharInput ()
+    let y = CharInput ()
+    [x;y]
 
 let rec NaturalInput () =
-    printf "Введите натуральное число: "
-    match System.Int32.TryParse(Console.ReadLine()) with
-    | (true, convertInt) when convertInt > 0 -> convertInt
-    | _ ->
+    printf "Введите количество повторений: "
+    let x = int(Console.ReadLine())
+    if (x<1) then
         printfn "Ошибка: Ненатуральное число"
         NaturalInput ()
-
-let rec NumberSum N =
-    if (N=0) then
-        N
     else
-        N%10 + NumberSum (N/10)
+        x
+
+let rec ListRepeat SummList OriginalList N =
+    if (N=0) then
+        SummList
+    else 
+        ListRepeat (SummList @ OriginalList) OriginalList (N-1)
+
 
 [<EntryPoint>]
 let main args =
-    printfn "Сумма цифр: %i" (NumberSum (NaturalInput ()))
+    printfn "Список: %A" (ListRepeat [] ListInput (NaturalInput ()))
     0
