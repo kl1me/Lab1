12cbfbd8988d67eef103be4d026c49385b8e87ca
kl1me <56569962+kl1me@users.noreply.github.com>
2026-02-27 08:19:21 +0500

    Lab1_2
    Lab1_2

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let rec NaturalInput () =
+    printf "Введите натуральное число: "
+    match System.Int32.TryParse(Console.ReadLine()) with
+    | (true, convertInt) when convertInt > 0 -> convertInt
+    | _ ->
+        printfn "Ошибка: Ненатуральное число"
+        NaturalInput ()
+
+let rec NumberSum N =
+    if (N=0) then
+        N
+    else
+        N%10 + NumberSum (N/10)
+
+[<EntryPoint>]
+let main args =
+    printfn "Сумма цифр: %i" (NumberSum (NaturalInput ()))
+    0
